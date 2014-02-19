397f51f41bfcfd018a0e4d7fe5f41dd2878968c7
Cail Borrell <cail@borrell.dk>
2014-02-19 22:07:20 +0100

    Default implementation of didMoveToWindow in UISlider does nothing. Added category method that calls super to trigger styling.

---
+++ Classy/Additions/UISlider+CASAdditions.m
@@ -0,0 +1,17 @@
+//
+//  UISlider+CASAdditions.m
+//  
+//
+//  Created by Cail Borrell on 19/02/14.
+//
+//
+
+#import "UISlider+CASAdditions.h"
+
+@implementation UISlider (CASAdditions)
+
+- (void)didMoveToWindow {
+    [super didMoveToWindow];
+}
+
+@end
