9a559973044ad8442af6fc29cd32d57440c4b02c
Ole Gammelgaard Poulsen <ole@shape.dk>
2014-04-09 11:30:26 +0200

    Revert "Added -cas_updateStylingSkipOffscreen: to allow force styling of a view even if it does not have a window."
    This reverts commit 78cb1a6fb5aeb0ffa6d9a9e8c6185c66b2d8b2cb.

--- Classy/Additions/UIView+CASAdditions.h
+++ Classy/Additions/UIView+CASAdditions.h
@@ -15,12 +15,4 @@
 
 - (void)cas_setNeedsUpdateStylingForSubviews;
 
-/**
- *  Same as cas_updateStyling, but by setting skipOffscreen to NO you can force styling of an
- *  item even if it is not added to a window yet.
- *  This can be useful for a number of reasons. For instance if you need to calculate the height
- *  of a UITableViewCell using AutoLayout and the layout depends on the font size in a label.
- */
-- (void)cas_updateStylingSkipOffscreen:(BOOL)skipOffscreen;
-
 @end
