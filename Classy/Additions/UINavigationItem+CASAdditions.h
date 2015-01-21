0ae2dae1e3dc430a33bce959b223cf92bbca5a4d
Joe <jridenour@mercury.io>
2015-01-21 10:12:20 -0600

    Fix for UIBarButtonItems not being styled in time
    A UIBarButtonItem that is being pushed onto a navigation stack does not get styled immediately.   The reason is the UINavigationBar is always on the screen, and doesn't get the didMoveToWindow method again,  so classy has no way of knowing that these items need styling.
    
    With this fix whenever you set the item(s) on the UINavigationItem it will do the styling immediately.

---
+++ Classy/Additions/UINavigationItem+CASAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationItem+CASAdditions.h
+//  
+//
+//  Created by Joseph Ridenour on 1/21/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationItem (CASAdditions)
+
+@end
